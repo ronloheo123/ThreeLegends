fddc37ee012a43328ac46cd22c13d0c7a8ba01d2
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-12-07 21:04:50 +0700

    NEWWWW

--- components/truong.tsx
+++ components/truong.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Box from "@mui/system/Box";
 type Props = {};
 
-const truong = () => {
-  return <div>truongg</div>;
+const Truong = () => {
+  <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+    This is a section container
+  </Box>;
 };
+
+export const truong = Truong;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Box } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function Home() {
       </Head>
       <main>
         <div className="Tin"></div>
-        <div className="Truong"></div>
+        <Box>
+          <Truo
+        </Box>
         <div className="Duy"></div>
       </main>
     </>
